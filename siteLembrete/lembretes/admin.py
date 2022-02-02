e0fbfe2edc8d206cb0ccf1ba4ade1c56158b086f
ailsonazevedo <61508532+ailsonazevedo@users.noreply.github.com>
2022-02-02 10:09:14 -0300

    add core css
    add core css and class LembreteAdmin in admin.py

--- siteLembrete/lembretes/admin.py
+++ siteLembrete/lembretes/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import *
 
 
-#class LembreteAdmin(admin.ModelAdmin):
-#    list_display = ('nome', 'data_lembrete', 'descricao')
-#   list_filter = ('data_lembrete')
+class LembreteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'data_lembrete', 'descricao')
+    list_filter = ('data_lembrete')
 
 # Register your models here.
 admin.site.register(Lembrete)
 admin.site.register(Perfil)
-
+#admin.site.register(LembreteAdmin)
